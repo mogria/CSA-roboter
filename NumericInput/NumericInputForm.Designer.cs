81a90b69599229421b1654e52cdd4da4674e6aef
Moritz Küttel <moritz.kuettel@hslu.ch>
2016-11-15 13:10:31 +0100

    add testing project for numeric input form

---
+++ NumericInput/NumericInputForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace NumericInput
+{
+    partial class NumericInputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberDisplay = new System.Windows.Forms.TextBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonNumber7 = new System.Windows.Forms.Button();
+            this.buttonNumber8 = new System.Windows.Forms.Button();
+            this.buttonNumber9 = new System.Windows.Forms.Button();
+            this.buttonNumber4 = new System.Windows.Forms.Button();
+            this.buttonNumber5 = new System.Windows.Forms.Button();
+            this.buttonNumber6 = new System.Windows.Forms.Button();
+            this.buttonNumber3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonNumber1 = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonNumber0 = new System.Windows.Forms.Button();
+            this.buttonPlusMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberDisplay
+            // 
+            this.numberDisplay.Location = new System.Drawing.Point(13, 13);
+            this.numberDisplay.Name = "numberDisplay";
+            this.numberDisplay.Size = new System.Drawing.Size(174, 20);
+            this.numberDisplay.TabIndex = 0;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(13, 40);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(82, 23);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "<<";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(101, 41);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(86, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "CE";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonNumber7
+            // 
+            this.buttonNumber7.Location = new System.Drawing.Point(13, 70);
+            this.buttonNumber7.Name = "buttonNumber7";
+            this.buttonNumber7.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber7.TabIndex = 3;
+            this.buttonNumber7.Text = "7";
+            this.buttonNumber7.UseVisualStyleBackColor = true;
+            this.buttonNumber7.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber8
+            // 
+            this.buttonNumber8.Location = new System.Drawing.Point(73, 70);
+            this.buttonNumber8.Name = "buttonNumber8";
+            this.buttonNumber8.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber8.TabIndex = 4;
+            this.buttonNumber8.Text = "8";
+            this.buttonNumber8.UseVisualStyleBackColor = true;
+            this.buttonNumber8.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber9
+            // 
+            this.buttonNumber9.Location = new System.Drawing.Point(133, 70);
+            this.buttonNumber9.Name = "buttonNumber9";
+            this.buttonNumber9.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber9.TabIndex = 5;
+            this.buttonNumber9.Text = "9";
+            this.buttonNumber9.UseVisualStyleBackColor = true;
+            this.buttonNumber9.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber4
+            // 
+            this.buttonNumber4.Location = new System.Drawing.Point(12, 127);
+            this.buttonNumber4.Name = "buttonNumber4";
+            this.buttonNumber4.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber4.TabIndex = 6;
+            this.buttonNumber4.Text = "4";
+            this.buttonNumber4.UseVisualStyleBackColor = true;
+            this.buttonNumber4.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber5
+            // 
+            this.buttonNumber5.Location = new System.Drawing.Point(73, 127);
+            this.buttonNumber5.Name = "buttonNumber5";
+            this.buttonNumber5.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber5.TabIndex = 7;
+            this.buttonNumber5.Text = "5";
+            this.buttonNumber5.UseVisualStyleBackColor = true;
+            this.buttonNumber5.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber6
+            // 
+            this.buttonNumber6.Location = new System.Drawing.Point(133, 127);
+            this.buttonNumber6.Name = "buttonNumber6";
+            this.buttonNumber6.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber6.TabIndex = 8;
+            this.buttonNumber6.Text = "6";
+            this.buttonNumber6.UseVisualStyleBackColor = true;
+            this.buttonNumber6.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber3
+            // 
+            this.buttonNumber3.Location = new System.Drawing.Point(133, 184);
+            this.buttonNumber3.Name = "buttonNumber3";
+            this.buttonNumber3.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber3.TabIndex = 11;
+            this.buttonNumber3.Text = "3";
+            this.buttonNumber3.UseVisualStyleBackColor = true;
+            this.buttonNumber3.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 51);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonNumber1
+            // 
+            this.buttonNumber1.Location = new System.Drawing.Point(12, 184);
+            this.buttonNumber1.Name = "buttonNumber1";
+            this.buttonNumber1.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber1.TabIndex = 9;
+            this.buttonNumber1.Text = "1";
+            this.buttonNumber1.UseVisualStyleBackColor = true;
+            this.buttonNumber1.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(100, 299);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(87, 23);
+            this.buttonCancel.TabIndex = 13;
+            this.buttonCancel.Text = "cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(12, 298);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(82, 23);
+            this.buttonOk.TabIndex = 12;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonNumber0
+            // 
+            this.buttonNumber0.Location = new System.Drawing.Point(12, 241);
+            this.buttonNumber0.Name = "buttonNumber0";
+            this.buttonNumber0.Size = new System.Drawing.Size(54, 51);
+            this.buttonNumber0.TabIndex = 14;
+            this.buttonNumber0.Text = "0";
+            this.buttonNumber0.UseVisualStyleBackColor = true;
+            this.buttonNumber0.Click += new System.EventHandler(this.buttonNumber_Click);
+            // 
+            // buttonPlusMinus
+            // 
+            this.buttonPlusMinus.Location = new System.Drawing.Point(73, 241);
+            this.buttonPlusMinus.Name = "buttonPlusMinus";
+            this.buttonPlusMinus.Size = new System.Drawing.Size(114, 51);
+            this.buttonPlusMinus.TabIndex = 15;
+            this.buttonPlusMinus.Text = "+/-";
+            this.buttonPlusMinus.UseVisualStyleBackColor = true;
+            this.buttonPlusMinus.Click += new System.EventHandler(this.buttonPlusMinus_Click);
+            // 
+            // NumericInputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 335);
+            this.Controls.Add(this.buttonPlusMinus);
+            this.Controls.Add(this.buttonNumber0);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonNumber3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonNumber1);
+            this.Controls.Add(this.buttonNumber6);
+            this.Controls.Add(this.buttonNumber5);
+            this.Controls.Add(this.buttonNumber4);
+            this.Controls.Add(this.buttonNumber9);
+            this.Controls.Add(this.buttonNumber8);
+            this.Controls.Add(this.buttonNumber7);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.numberDisplay);
+            this.Name = "NumericInputForm";
+            this.Text = "NumberFormatInput";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numberDisplay;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonNumber7;
+        private System.Windows.Forms.Button buttonNumber8;
+        private System.Windows.Forms.Button buttonNumber9;
+        private System.Windows.Forms.Button buttonNumber4;
+        private System.Windows.Forms.Button buttonNumber5;
+        private System.Windows.Forms.Button buttonNumber6;
+        private System.Windows.Forms.Button buttonNumber3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonNumber1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonNumber0;
+        private System.Windows.Forms.Button buttonPlusMinus;
+    }
+}
+
---
+++ NumericInput/NumericInputForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumericInput
+{
+    public partial class NumericInputForm : Form
+    {
+
+        private int numberEntered;
+
+        public int NumberEntered
+        {
+            get { return numberEntered; }
+            set {
+                numberDisplay.Text = value + "";
+                numberEntered = value;
+            }
+        }
+
+        public NumericInputForm()
+        {
+            InitializeComponent();
+            NumberEntered = 0;
+
+        }
+
+        private void buttonNumber_Click(object sender, EventArgs e)
+        {
+            if(sender is Button) {
+                Button numberButton = sender as Button;
+                int number = 0;
+                if(int.TryParse(numberButton.Text, out number))
+                {
+                    NumberEntered *= 10;
+                    NumberEntered += number;
+                }
+            }
+        }
+
+        private void buttonPlusMinus_Click(object sender, EventArgs e)
+        {
+            NumberEntered *= -1;
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            NumberEntered /= 10;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            NumberEntered = 0;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
