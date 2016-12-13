93da4b73db0c4522e4e29574a8212dc9c15a2e94
Moritz Küttel <moritz.kuettel@hslu.ch>
2016-12-13 18:52:08 +0100

    add HttpCommander project for controlling the Robot via HTTP

---
+++ HttpCommander/HttpServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace HttpCommander
+{
+    class HttpServer
+    {
+        private TcpListener tcpListener;
+        public HttpServer()
+        {
+            tcpListener = new TcpListener(8080);
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+
+            while(true)
+            {
+                Console.WriteLine("Waiting for connections ... ");
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                Console.WriteLine("Got connection from: " + tcpClient.ToString());
+                var handler = new HttpHandler(tcpClient);
+                new Thread(handler.HandleRequest).Start();
+            }
+        }
+    }
+}
